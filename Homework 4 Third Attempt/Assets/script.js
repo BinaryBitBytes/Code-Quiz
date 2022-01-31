ac43a24ace36a438e1f01d7b611f5589c4ebcf07
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-01-31 11:33:38 -0500

    added jqueryscript.js, modified script.js, modified index.html

--- Homework 4 Third Attempt/Assets/script.js
+++ Homework 4 Third Attempt/Assets/script.js
@@ -1,1 +1,2 @@
 var x = document.getElementByClass("button");
+
---
+++ assets/jqueryscript.js
@@ -0,0 +1,14 @@
+$.getJSON("/questions.json",function(questions){
+    var question =[];
+    $.each( questions, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+     
+      $( "<ul/>", {
+        "class": "quizQuestions",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    }); //JSON.stringify(questions);
+    console.log(questions[1,2,3,4].QUESTION)
+    $('#question').append(question)
+})
